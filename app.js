a1c12f04e103b730d2a5e80caa9f8c9cec92b041
Athira.V.Ajit <42166057+Athira2199@users.noreply.github.com>
2021-01-21 12:42:13 +0530

    adding initial skeleton page of site

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const express=require('express')
+const bodyParse=require('body-parser')
+const app     = express()
+
+const PORT = 4000
+
+app.set('view engine','ejs')
+app.use(express.static(path.join(__dirname,'public')))
+
+
+
+app.use('/',require('./routes/index'))
+app.use('/login',require('./routes/login'))
+app.use('/register',require('./routes/register'))
+app.use('/category',require('./routes/category'))
+
+
+app.use('*',(req,res)=>{
+    res.send('coming soon')
+})
+
+app.listen(PORT,console.log(`listening to port ${PORT}`))
